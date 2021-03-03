edcc81de0974559902f690bf616dd382739e5773
Reagan Soltis <you@example.com>
2021-03-02 18:07:38 -0600

    adds nodemon functionality

--- index.ts
+++ index.ts
@@ -6,14 +6,14 @@ const git: SimpleGit = simpleGit();
 // it will add the fargate-files folder
 // send to a remote url
 function build() {
-    git.checkoutLocalBranch('sending-files');
+    git.checkoutLocalBranch('sending-to-remote');
     git.branch(['--orphan']);
     git.rm(['--cached', '-r', '.']);
     git.add('fargate-files');
     git.commit('sending fargate files to commit repo!!');
     // will add repo url as variable
     git.addRemote('test', 'https://tfs-svm.visualstudio.com/UA-Capstone-2021/_git/terraform-fargate-test');
-    git.push(['-u', 'test', 'sending-files'], () => console.log('done'));
+    git.push(['-u', 'test', 'sending-to-remote'], () => console.log('done'));
   // console.log(git.status());
 } 
 build();
