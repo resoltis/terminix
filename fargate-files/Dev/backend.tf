665edc1e86f8d67767fa4167c4b5d4340a291cb9
Jared Waller <jwinala31399@yahoo.com>
2021-03-02 20:20:12 -0600

    sending fargate files to commit repo!!

--- fargate-files/Dev/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-	backend "s3" {
-		bucket  = "tf-svm-state-files-prod"
-		key     = "TMX-BI-Dev/salesforceworkorderkafkaconsumer/ecs_fargate_stack/terraform.tfstate"
-		region  = "us-east-1"
-		profile = "obiwanprod"
-	}
-}
