9ec8ae73a21a95dc43a615c888b3ac183152a299
Reagan Soltis <you@example.com>
2021-03-31 22:57:54 -0500

    updates index.ts

--- index.ts
+++ index.ts
@@ -3,17 +3,20 @@ import simpleGit, {SimpleGit} from 'simple-git';
 const git: SimpleGit = simpleGit();
 
 // this will create a new branch and clear the local stashes
-// it will add the fargate-files folder
+// it will add the CFT Files folder
 // send to a remote url
 function build() {
-   // git.checkoutLocalBranch('sending-to-remote');
-   // git.branch(['--orphan']);
-   // git.rm(['--cached', '-r', '.']);
-   // git.add('fargate-files');
-   // git.commit('sending fargate files to commit repo!!');
-    // will add repo url as variable
-   // git.addRemote('test', 'https://tfs-svm.visualstudio.com/UA-Capstone-2021/_git/terraform-fargate-test');
-   // git.push(['-u', 'test', 'sending-to-remote'], () => console.log('done'));
-  // console.log(git.status());
+  git.rm(['--cached','-r','.']);
+  git.checkoutLocalBranch('sending-to-remote-CFT');
+  git.branch(['--orphan']);
+  git.clean('-f');
+  git.add('CFT Files');
+  git.commit('sending cft to commit repo!!');
+  // will add repo url as variable
+  git.addRemote('test', 'https://tfs-svm.visualstudio.com/UA-Capstone-2021/_git/terraform-fargate-test');
+  git.push(['-u','test', 'sending-to-remote-CFT'], () => console.log('done'));
+  git.init();
+  git.checkoutBranch('main', 'origin');
+  git.reset(['HEAD^', '--hard']);
 } 
 build();
