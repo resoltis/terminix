40b74006f78b641f1e3753288347c54eec49d47b
Reagan Soltis <you@example.com>
2021-04-12 11:56:59 -0500

    adds comments and cleans index.ts file

--- index.ts
+++ index.ts
@@ -2,20 +2,17 @@ import simpleGit, {SimpleGit} from 'simple-git';
 
 const git: SimpleGit = simpleGit();
 
-// this will create a new branch and clear the local stashes
-// it will add the CFT Files folder
-// send to a remote url
+// this will create a new branch and clear all untracked files
+// it will add the CFT Files folder when it is created
+// add a commit message and add a remote repository variable surveyed by the user
+// push the new branch to the remote repo
 function build() {
   git.checkoutLocalBranch('sending-to-remote-CFT');
-  git.branch(['--orphan']);
   git.clean('-f');
   git.add('CFT Files');
   git.commit('sending cft files to a remote repository');
   // will add repo url as variable
   git.addRemote('test', 'https://tfs-svm.visualstudio.com/UA-Capstone-2021/_git/terraform-fargate-test');
   git.push(['-u','test', 'sending-to-remote-CFT'], () => console.log('done'));
-  //git.init();
-  //git.checkoutBranch('main', 'origin');
-  //git.reset(['HEAD^', '--hard']);
 } 
 build();
